cf5c352ff2bb305c61a85e1d3a22c07fef03627f
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-02-12 12:23:55 +0700

    test

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,14 +3,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/test")
-    String test()
+    @PostMapping("/webhook")
+    String test(@RequestBody String body)
     {
-        System.out.println("Test ok!!");
+        System.out.println(body);
         return "OKK";
     }
 }
